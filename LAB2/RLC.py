e8325a4474cf3cb31833863e0df9648b8e851d03
aww-sam <guragaisamyog30@gmail.com>
2026-08-06 01:17:35 +0545

    discharging)of_capacitor

---
+++ LAB2/RLC.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+R=20
+L=1
+C=0.01
+v0=10
+alpha=R/(2*L)
+w0=1/((L*C)**0.5)
+
+t=np.linspace(0,10,100000)
+if alpha>w0:
+    print("overdamped")
+    s1=-alpha+np.sqrt(alpha**2-w0**2)
+    s2=-alpha-np.sqrt(alpha**2-w0**2)
+    i=((v0/(L*(s1-s2)))*np.exp(s1*t))+((-v0/(L*(s1-s2)))*np.exp(s2*t))
+    v=((v0/(s1-s2)))*((s1*np.exp(s2*t))-(s2*np.exp(s1*t)))  
+elif alpha==w0:
+    print("criticall")
+    i=(v0/L)*np.exp(-alpha*t)*t
+    v=v0*(1+(alpha*t))*np.exp(-alpha*t)
+else:
+    print("underdamped")
+    Wd=(w0**2-alpha**2)**0.5
+    sin_term=np.sin(Wd*t)
+    cos_term=np.cos(wd*t)
+    i=(v0/(L*Wd))*(np.exp(-alpha*t))*sin_term
+    v=v0*np.exp(-alpha*t)*(cos_term+((alpha/Wd)*sin_term))
+
+plt.plot(t,i,c='r',label='current')
+plt.plot(t,v,c='b',label='voltage')
+plt.legend()
+plt.grid()
+plt.show()
