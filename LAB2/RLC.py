d0265504a1b94e07a31204b66b461e531ebd67bd
aww-sam <guragaisamyog30@gmail.com>
2026-08-07 02:14:09 +0545

    few_changes

--- LAB2/RLC.py
+++ LAB2/RLC.py
@@ -14,11 +14,14 @@ if alpha>w0:
     s1=-alpha+np.sqrt(alpha**2-w0**2)
     s2=-alpha-np.sqrt(alpha**2-w0**2)
     i=((v0/(L*(s1-s2)))*np.exp(s1*t))+((-v0/(L*(s1-s2)))*np.exp(s2*t))
-    v=((v0/(s1-s2)))*((s1*np.exp(s2*t))-(s2*np.exp(s1*t)))  
+    v=((v0/(s1-s2)))*((s1*np.exp(s2*t))-(s2*np.exp(s1*t))) 
+    plt.title("Overdamped RLC circuit")
 elif alpha==w0:
     print("criticall")
     i=(v0/L)*np.exp(-alpha*t)*t
     v=v0*(1+(alpha*t))*np.exp(-alpha*t)
+    plt.title("Critically damped RLC circuit")
+
 else:
     print("underdamped")
     Wd=(w0**2-alpha**2)**0.5
@@ -26,6 +29,7 @@ else:
     cos_term=np.cos(Wd*t)
     i=(v0/(L*Wd))*(np.exp(-alpha*t))*sin_term
     v=v0*np.exp(-alpha*t)*(cos_term+((alpha/Wd)*sin_term))
+    plt.title("Underdamped RLC circuit")
 
 plt.plot(t,i,c='r',label='current')
 plt.plot(t,v,c='b',label='voltage')
